3dea8e0171e9b67e97d01dda39d675767d803b04
Rudy Schneider <rks9pax@virginia.edu>
2021-09-20 15:15:27 -0400

    spotify authentication skeleton

--- App.js
+++ App.js
@@ -2,10 +2,47 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppLogin from '../spotifacts/components/Screens/AppLogin/AppLogin';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { Button } from 'react-native';
+import { useEffect } from "react";
+
+//Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
 
 export default function App() {
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: '184c8af0f1e440d4a2682ff3f6ec63cc',
+      scopes: ['user-read-email', 'playlist-modify-public'],
+      // In order to follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
+      // this must be set to false
+      usePKCE: false,
+      // For usage in managed apps using the proxy
+      redirectUri: makeRedirectUri({
+        // For usage in bare and standalone
+        native: 'exp://localhost:19000/',
+      }),
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      }
+  }, [response]);
+
   return (
-    <AppLogin/>
+    <Button
+      disabled={!request}
+      title="Login"
+      onPress={() => {
+        promptAsync();
+        }}
+    />
   );
 }
 
